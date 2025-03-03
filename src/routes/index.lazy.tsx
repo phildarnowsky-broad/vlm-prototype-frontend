68a1da1522d53f2340311d95c16a2e88aae8b043
Phil Darnowsky <pdarnows@broadinstitute.org>
2025-03-03 14:34:24 -0500

    get typechecking working, fix a few bugs thus revealed

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
   QueryClientProvider,
 } from "@tanstack/react-query";
-import { useState } from "react";
+import React, { useState } from "react";
 
 interface ResultSet {
   id: string;
